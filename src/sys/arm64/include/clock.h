3853c614154bb4da5d6a4e4bb553d9af5dd2e7de
gast <henning.matyschok@stud.fh-flensburg.de>
2016-08-20 00:03:02 +0200

    arm64 based implementation removed.

--- src/sys/arm64/include/clock.h
+++
@@ -1,1 +0,0 @@
-/* $FreeBSD: head/sys/arm64/include/clock.h 281494 2015-04-13 14:43:10Z andrew $ */
--- src/sys/arm64/include/trap.h
+++
@@ -1,1 +0,0 @@
-/* $FreeBSD: head/sys/arm64/include/trap.h 281494 2015-04-13 14:43:10Z andrew $ */
